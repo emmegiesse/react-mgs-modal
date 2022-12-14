7d7833532584b33ceacb14e2c2b02740673c3222
emmegiesse <martagiulia.scoditti@gmail.com>
2022-12-14 23:10:23 +0100

    build 2

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _Modal = _interopRequireDefault(require("./components/Modal.js"));
+var _useModal = _interopRequireDefault(require("./style/useModal.js"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _default = {
+  Modal: _Modal.default,
+  useModal: _useModal.default
+};
+exports.default = _default;
