977cdaedd7dea2871750c2877e40e08fc55484f1
emmegiesse <martagiulia.scoditti@gmail.com>
2022-12-14 21:27:00 +0100

    babel creation

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _Modal = _interopRequireDefault(require("./components/Modal.js"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _default = _Modal.default;
+exports.default = _default;
